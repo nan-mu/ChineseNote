fcc7520f34771b8859f37dbf52b035949f5c08a3
nan-mu <nan_mu_fan_ren@hotmail.com>
2018-10-06 16:36:17 +0800

    add a lot of layout in navigation.html and news.html

---
+++ news/news.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    $("#s1").css("background-image", "url(img/smallPhoto-" + parseInt(Math.random() * 16 + 1) + ".jpg");
+    $("#s2").css("background-image", "url(img/smallPhoto-" + parseInt(Math.random() * 16 + 1) + ".jpg");
+    $("#s4").css("background-image", "url(img/smallPhoto-" + parseInt(Math.random() * 16 + 1) + ".jpg");
+});
