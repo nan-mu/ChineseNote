193edcb369c36d2f4be620d3cdd6bf0850ef2752
木 楠 <nan_mu_fan_ren@hotmail.com>
2018-12-20 18:23:16 +0800

    Change the layout at blog-chinese-people.SuShi.html

--- subject/chinese/js/service-carousel.js
+++ subject/chinese/js/service-carousel.js
@@ -4,12 +4,12 @@ $(document).on('ready', function() {
     "use strict";
     $(".service").owlCarousel({
 
-        autoPlay: 3000, //Set AutoPlay to 3 seconds
+        autoPlay: 3000, //Set AutoPlay to 1 seconds
         navigation: true, // Show next and prev buttons
         navigationText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
         items: 3,
         itemsDesktop: [1199, 3],
-        itemsDesktopSmall: [979, 3],
+        itemsDesktopSmall: [979,3],
         pagination: false
 
     });
