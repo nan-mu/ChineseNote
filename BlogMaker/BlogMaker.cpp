7d540c6aebd41627930834d3842d1372f969e06f
木 楠 <nan_mu_fan_ren@hotmail.com>
2018-12-21 21:17:45 +0800

    fined a bug at SuShi's 获见帖

--- BlogMaker/BlogMaker.cpp
+++ BlogMaker/BlogMaker.cpp
@@ -1,12 +1,69 @@
-﻿// BlogMaker.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+﻿// tinyTool.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 //
 
 #include "pch.h"
-#include <iostream>
+using namespace std;
+
+string Lpcwstr2String(LPCWSTR lps);
+string get_path();
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	fstream  Fenglish, Fchina, Fout;
+	printf("选择英文单词文件");
+	Fenglish.open(get_path(), ios::in);
+	printf("\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b选择中文翻译文件\n");
+	Fchina.open(get_path(), ios::in);
+
+	Fout.open("d:\\a.txt", ios::app);
+	string BeWorkWord; int BeWordLanguage = 0;
+	while (getline(Fenglish, BeWorkWord)) {
+		Fout << BeWorkWord;
+		for (int i = 0; i < interval; i++)Fout << '。';
+		getline(Fchina, BeWorkWord);
+		Fout << BeWorkWord;
+		for (int i = 0; i < interval; i++)Fout << '。';
+		Fout << endl;
+	}
+	Fchina.close();
+	Fenglish.close();
+	Fout.close();
+	printf("编排完成");
+}
+
+string Lpcwstr2String(LPCWSTR lps) {//将一个LPCWSTR转换为string
+	int len = WideCharToMultiByte(CP_ACP, 0, lps, -1, NULL, 0, NULL, NULL);
+	if (len <= 0) { return ""; }
+	else {
+		char *dest = new char[len];
+		WideCharToMultiByte(CP_ACP, 0, lps, -1, dest, len, NULL, NULL);
+		dest[len - 1] = 0;
+		string str(dest);
+		delete[] dest;
+		return str;
+	}
+}
+
+string get_path() {//新建一个对话窗口，选择文件
+	OPENFILENAME ofn;	char szFile[300];
+	ZeroMemory(&ofn, sizeof(ofn));
+	ofn.lStructSize = sizeof(ofn);
+	ofn.hwndOwner = NULL;
+	ofn.lpstrFile = (LPWSTR)szFile;
+	ofn.lpstrFile[0] = '\0';
+	LPTSTR lpstrCustomFilter;
+	DWORD nMaxCustFilter;
+	ofn.nFilterIndex = 1;
+	LPTSTR lpstrFile;
+	ofn.nMaxFile = sizeof(szFile);
+	ofn.lpstrFilter = L"ALL\0*.*\0Text\0*.TXT\0";
+	ofn.lpstrFileTitle = NULL;
+	ofn.nMaxFileTitle = 0;
+	ofn.lpstrInitialDir = NULL;
+	ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
+	string path = "";
+	if (GetOpenFileName(&ofn)) { path = Lpcwstr2String(ofn.lpstrFile);		return path; }
+	else { return ""; }
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
